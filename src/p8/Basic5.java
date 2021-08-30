7504f684d927c8700f20dc1cb33692b02d961e86
Maria Zinchyn <mariazinchyn@gmail.com>
2021-08-30 16:22:09 +0300

    Basic5

---
+++ src/p8/Basic5.java
@@ -0,0 +1,16 @@
+package p8;
+
+public class Basic5 {
+
+        public static String greet(String name) {
+
+
+            if(name.equals("Johnny")){
+                return "Hello, my love!";
+            } else{
+                return String.format("Hello, %s!", name);
+
+            }
+        }
+    }
+
