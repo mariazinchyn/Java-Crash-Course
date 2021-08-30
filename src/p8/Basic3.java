49c13b0ba06c62d7ecd6a56a647082721fbfe30e
Maria Zinchyn <mariazinchyn@gmail.com>
2021-08-30 16:12:11 +0300

    Basic3

---
+++ src/p8/Basic3.java
@@ -0,0 +1,8 @@
+package p8;
+
+public class Basic3 {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        double volume = length * width * height;
+        return volume;
+    }
+}
