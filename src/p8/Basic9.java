c2b3efec07f1d95d7b719241bc2df9fb04ec9549
Maria Zinchyn <mariazinchyn@gmail.com>
2021-08-30 17:06:09 +0300

    Basic9

---
+++ src/p8/Basic9.java
@@ -0,0 +1,12 @@
+package p8;
+
+public class Basic9 {
+    public static int makeNegative(final int x) {
+        if(x < 0){
+            return x;
+        }else{
+            return -1 * x;
+        }
+
+    }
+}
