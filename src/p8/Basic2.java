a23ac512deca7afff00ba7c039824c73f7b72b86
Maria Zinchyn <mariazinchyn@gmail.com>
2021-08-30 15:59:43 +0300

    Basic2

--- src/p8/Basic2.java
+++ src/p8/Basic2.java
@@ -1,10 +1,8 @@
 package p8;
-//https://www.codewars.com/kata/function-3-multiplying-two-numbers/
 
-//Implement a function which multiplies two numbers.
 public class Basic2 {
     public static int multiply(int num1, int num2) {
-        // your code goes here
+
         return num1*num2;
     }
 }
